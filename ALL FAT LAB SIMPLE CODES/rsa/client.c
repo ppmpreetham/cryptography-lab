aa0cf0da21b02fb8cd8bb91f9dc2351a0ee4d3f3
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-04-13 01:00:18 +0530

    fixed rsa

--- ALL FAT LAB SIMPLE CODES/rsa/client.c
+++ ALL FAT LAB SIMPLE CODES/rsa/client.c
@@ -3,61 +3,52 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
 #define W(fd, msg) write(fd, msg, strlen(msg))
 #define R(fd, buf) {n = read(fd, buf, 1024); buf[n] = 0;}
-
 #define W_INT(fd, msg) {int t = htonl(msg); write(fd, &t, sizeof(t));}
 #define R_INT(fd, msg) {int t; read(fd, &t, sizeof(t)); msg = ntohl(t);}
-
 int modinv(int n, int p){
     int res = 1;
-    while(n * res % p != 1){
-        res++;
-    }
+    while(n * res % p != 1) res++;
     return res;
 }
-
 int gcd(int a, int b){
     if (b == 0) return a;
     return gcd(b, a % b);
 }
-
 int totient(int n){
     int cnt = 0;
-    for(int i = 1; i < n; i++){
-        if (gcd(n,i) == 1){
-            cnt++;
-        }
-    }
+    for(int i = 1; i < n; i++)
+        if (gcd(n,i) == 1) cnt++;
     return cnt;
 }
-
-// a^b mod p
 int modexp(int a, int b, int p){
-    if (p == 0) return -1;  
     int res = 1;
     a = a % p;
     while (b > 0){
-        if (b % 2 ==1) res = (res * a ) % p;
+        if (b % 2 == 1) res = (res * a) % p;
         b = b/2;
         a = (a * a) % p;
     }
     return res;
 }
-
 int main(){
     int s = socket(2,1,0), c, n;
-    struct sockaddr_in a = {2,htons(8080),0};
-    int b[1024];
+    struct sockaddr_in a;
+    a.sin_family = AF_INET;
+    a.sin_port = htons(8080);
+    a.sin_addr.s_addr = inet_addr("127.0.0.1");
     connect(s, (struct sockaddr*)&a, 16);
     int e, n_val;
     R_INT(s, e);
     R_INT(s, n_val);
-    
     while(1){
         int m;
         scanf("%d", &m);
+        if(m <= 0 || m >= n_val){
+            printf("Message must be between 1 and %d\n", n_val-1);
+            continue;
+        }
         int enc = modexp(m,e,n_val);
         W_INT(s, enc);
     }
--- ALL FAT LAB SIMPLE CODES/rsa/server.c
+++ ALL FAT LAB SIMPLE CODES/rsa/server.c
@@ -3,48 +3,35 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
 #define W(fd, msg) write(fd, msg, strlen(msg))
 #define R(fd, buf) {n = read(fd, buf, 1024); buf[n] = 0;}
-
 #define W_INT(fd, msg) {int t = htonl(msg); write(fd, &t, sizeof(t));}
 #define R_INT(fd, msg) {int t; read(fd, &t, sizeof(t)); msg = ntohl(t);}
-
 int modinv(int n, int p){
     int res = 1;
-    while(n * res % p != 1){
-        res++;
-    }
+    while(n * res % p != 1) res++;
     return res;
 }
-
 int gcd(int a, int b){
     if (b == 0) return a;
     return gcd(b, a % b);
 }
-
 int totient(int n){
     int cnt = 0;
-    for(int i = 1; i < n; i++){
-        if (gcd(n,i) == 1){
-            cnt++;
-        }
-    }
+    for(int i = 1; i < n; i++)
+        if (gcd(n,i) == 1) cnt++;
     return cnt;
 }
-
-// a^b mod p
 int modexp(int a, int b, int p){
     int res = 1;
-     a = a % p;
+    a = a % p;
     while (b > 0){
-        if (b % 2 ==1) res = (res * a ) % p;
+        if (b % 2 == 1) res = (res * a) % p;
         b = b/2;
         a = (a * a) % p;
     }
     return res;
 }
-
 int main(){
     int s = socket(2,1,0), c, n;
     struct sockaddr_in a = {2,htons(8080),0};
@@ -52,26 +39,20 @@ int main(){
     bind(s, (struct sockaddr*)&a, 16);
     listen(s, 5);
     c = accept(s, 0, 0);
-
-    int p = 3;
+    int p = 7;
     int q = 11;
     int n_val = p*q;
     int tot = totient(n_val);
     int e = 7;
-
-    if (gcd(e, tot) != 1){
-        printf("not right e");
-    }
-
+    if (gcd(e, tot) != 1) printf("not right e");
     int d = modinv(e, tot);
     W_INT(c,e);
     W_INT(c,n_val);
     int enc;
-    while (1){
+    while(1){
         R_INT(c, enc);
-        break;
+        if(enc == -1) break;
+        int dec = modexp(enc,d,n_val);
+        printf("%d\n", dec);
     }
-
-    int dec = modexp(enc,d,n_val);
-    printf("%d\n", dec);
 }
